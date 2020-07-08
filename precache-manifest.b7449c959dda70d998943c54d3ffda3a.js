ee0fcee72d37f30fa3c8718154fcb9e6dd27551c
itacamba <daniela.sahagun03@gmail.com>
2020-07-08 17:22:25 -0400

    Updates

---
+++ precache-manifest.b7449c959dda70d998943c54d3ffda3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b740f51ab77c1280b31d35a1f30a168b",
+    "url": "/bcs-deploy-test/index.html"
+  },
+  {
+    "revision": "0f4c51f7e1622e638bd8",
+    "url": "/bcs-deploy-test/static/css/main.27c8d419.chunk.css"
+  },
+  {
+    "revision": "9c67d5afad79f5185002",
+    "url": "/bcs-deploy-test/static/js/2.1c2127ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bcs-deploy-test/static/js/2.1c2127ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4c51f7e1622e638bd8",
+    "url": "/bcs-deploy-test/static/js/main.f90f0a8b.chunk.js"
+  },
+  {
+    "revision": "8d67a28408820ccc0914",
+    "url": "/bcs-deploy-test/static/js/runtime-main.b3da0f93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bcs-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
